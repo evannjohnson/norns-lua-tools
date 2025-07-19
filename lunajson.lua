219589b87c4aec622b97ffb2edfc2ae8a2647a65
Evan Johnson <ev.jonnson@gmail.com>
2025-07-19 13:53:49 -0600

    change lunajson paths

--- lunajson.lua
+++ lunajson.lua
@@ -1,6 +1,6 @@
-local newdecoder = require 'lunajson.decoder'
-local newencoder = require 'lunajson.encoder'
-local sax = require 'lunajson.sax'
+local newdecoder = require 'tools.lunajson.decoder'
+local newencoder = require 'tools.lunajson.encoder'
+local sax = require 'tools.lunajson.sax'
 -- If you need multiple contexts of decoder and/or encoder,
 -- you can require lunajson.decoder and/or lunajson.encoder directly.
 return {
